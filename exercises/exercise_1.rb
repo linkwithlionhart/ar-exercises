1f1e6ca73b2a6a55211f6e04858655cc865febe3
linkwithlionhart <linkwithlionhart@gmail.com>
2023-12-08 19:58:16 -0800

    adjust comments

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,5 +1,10 @@
+require_relative '../setup'
+
+puts "Exercise 1"
+puts "----------"
+
 =begin
-  Exercise 1: Create 3 stores
+Exercise 1: Create 3 stores
   Use Active Record's create class method multiple times to create 3 stores in the database:
     Burnaby (annual_revenue of 300000, carries men's and women's apparel)
     Richmond (annual_revenue of 1260000 carries women's apparel only)
@@ -7,12 +12,6 @@
   Output (puts) the number of the stores using ActiveRecord's count method, to ensure that there are three stores in the database.
 =end
 
-require_relative '../setup'
-
-puts "Exercise 1"
-puts "----------"
-
-# Your code goes below here ...
 store_1 = Store.create(
   name: 'Burnaby',
   annual_revenue: 300000,
--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,17 +1,16 @@
-=begin
-  Exercise 2: Update the first store
-    Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
-    Load the second store from the database and assign it to @store2.
-    Update the first store (@store1) instance in the database. (Change its name or something.)
-=end 
-
 require_relative '../setup'
 require_relative './exercise_1'
 
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+=begin
+Exercise 2: Update the first store
+  Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+  Load the second store from the database and assign it to @store2.
+  Update the first store (@store1) instance in the database. (Change its name or something.)
+=end 
+
 @store1 = Store.find(1)
 @store2 = Store.find(2)
 
