edf28b4d7eca6e12fb1b588ecdc1ff6182761566
linkwithlionhart <linkwithlionhart@gmail.com>
2023-12-08 20:50:39 -0800

    complete ex6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,40 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+=begin
+Exercise 6: One-to-many association
+  We haven't used the Employee class (and employees table) at all yet.
+  If you look at this table's column structure, you'll find that it has a store_id (integer) column.
+  This is a column that identifies which store each employee belongs to.
+  It points to the id (integer) column of their store.
+  Let's tell Active Record that these two tables are in fact related via the store_id column.
+    1. Add the following code directly inside the Store model (class): has_many :employees
+    2. Add the following code directly inside the Employee model (class): belongs_to :store
+    3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): 
+      @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+    4. Go ahead and create some more employees using the create method. 
+       You can do this by making multiple calls to create (like you have before.) 
+       No need to assign the employees to variables though. 
+       Create them through the @store# instance variables that you defined in previous exercises.
+       Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+=end
+
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 50)
+@store2.employees.create(first_name: "Jane", last_name: "Smith", hourly_rate: 55)
+@store2.employees.create(first_name: "William", last_name: "Lionhart", hourly_rate: 45)
+
+puts @store1.employees.count
+puts @store2.employees.count
+p @store1.employees
+
