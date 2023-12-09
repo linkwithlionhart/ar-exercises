20940d8950425949d02212ac46136dc28044c287
linkwithlionhart <linkwithlionhart@gmail.com>
2023-12-08 19:40:38 -0800

    add comments

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,3 +1,10 @@
+=begin
+  Exercise 2: Update the first store
+    Load the first store (with id = 1) from the database and assign it to an instance variable @store1.
+    Load the second store from the database and assign it to @store2.
+    Update the first store (@store1) instance in the database. (Change its name or something.)
+=end 
+
 require_relative '../setup'
 require_relative './exercise_1'
 
